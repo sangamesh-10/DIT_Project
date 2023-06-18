fa993d529595fa83ff4f53fa0946e01c16bb4afd
Your Name <you@example.com>
2023-06-18 17:00:46 +0530

    OTP and Update pwd

--- app/Http/Controllers/updationsController.php
+++ app/Http/Controllers/updationsController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Session;
-use App\Models\student;
+use App\Models\faculty_login;
+use App\Models\students_login;
 use Illuminate\Console\View\Components\Alert;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -11,9 +12,9 @@ class updationsController extends Controller
 {
     function stdPassword(Request $req)
     {
-        $username = $req->input('username');
-        //$currentPwd = $req->input('currentPwd');
+        $student_id = Session::get('user');
         $new_password = $req->input('new_password');
+        $confirm_password = $req->input('confirm_password');
 
         $rules = [
             'new_password' => 'required|regex:/^(?=.*[A-Z])(?=.*\d).{8,}$/'
@@ -21,16 +22,45 @@ class updationsController extends Controller
 
         $validator = Validator::make($req->all(), $rules);
 
-        if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator);
-        } else {
-            $student = student::where('student_id', $username)->first();
+        if ($validator->fails() || $new_password != $confirm_password) {
+            return redirect()->back()->with('error', 'Password format is not valid . Please try again.');
+        }
+        else
+        {
+            $student = students_login::where('student_id', $student_id)->first();
 
             if ($student) {
                 $student->password = $new_password;
                 $student->save();
 
-                return "Password updated successfully.";
+                return redirect()->route('studentLogin')->with('success', 'Password updated successfully. Please login with your new password.');;;
+            }
+        }
+    }
+    function facultyPassword(Request $req)
+    {
+        $faculty_id = Session::get('user');
+        $new_password = $req->input('new_password');
+        $confirm_password = $req->input('confirm_password');
+
+        $rules = [
+            'new_password' => 'required|regex:/^(?=.*[A-Z])(?=.*\d).{8,}$/'
+        ];
+
+        $validator = Validator::make($req->all(), $rules);
+
+        if ($validator->fails() || $new_password != $confirm_password) {
+            return redirect()->back()->with('error', 'Password format is not valid . Please try again.');
+        }
+        else
+        {
+            $faculty = faculty_login::where('faculty_id', $faculty_id)->first();
+
+            if ($faculty) {
+                $faculty->password = $new_password;
+                $faculty->save();
+
+                return redirect()->route('facultyLogin')->with('success', 'Password updated successfully. Please login with your new password.');
             }
         }
     }
@@ -40,6 +70,17 @@ class updationsController extends Controller
         if($user_otp==$actual_otp){
 
             return view('UpdatePassword');
+        }
+        else{
+            return redirect()->back()->with('error', 'Invalid OTP entered. Please try again.');
+        }
+    }
+    function otpVerificationFaculty( Request $req){
+        $user_otp = $req->input('otp');
+        $actual_otp =Session::get('otp');
+        if($user_otp==$actual_otp){
+
+            return view('UpdatePassword_Faculty');
         }
         else{
             return redirect()->back()->with('error', 'Invalid OTP entered. Please try again.');
--- resources/views/OtpVerification_Faculty.blade.php
+++ resources/views/OtpVerification_Faculty.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Student Login Page</title>
+    <title>Reset Password</title>
     @include('bootstrap')
     <link rel="stylesheet" href="{{ asset('css/basicStyle.css') }}">
 
@@ -11,7 +12,7 @@
             font-family: Arial, sans-serif;
         }
 
-        .loginform {
+        .verification-form {
             width: 300px;
             margin: auto;
             background-color: #ffffff;
@@ -21,13 +22,13 @@
             text-align: center;
         }
 
-        .loginform h2 {
+        .verification-form h2 {
             font-size: 24px;
             margin-bottom: 20px;
             color: #333333;
         }
 
-        .loginform label {
+        .verification-form label {
             font-size: 16px;
             font-weight: bold;
             display: block;
@@ -36,8 +37,7 @@
             text-align: left;
         }
 
-        .loginform input[type="text"],
-        .loginform input[type="password"] {
+        .verification-form input[type="text"] {
             width: 100%;
             padding: 10px;
             font-size: 16px;
@@ -46,7 +46,7 @@
             margin-bottom: 20px;
         }
 
-        .loginform input[type="submit"] {
+        .verification-form input[type="submit"] {
             width: 100%;
             padding: 10px;
             font-size: 16px;
@@ -56,41 +56,28 @@
             border-radius: 5px;
             cursor: pointer;
         }
-
-        .loginform .forgot-password {
-            font-size: 14px;
-            color: #333333;
-            margin-top: 10px;
-        }
     </style>
 </head>
+
 <body>
     <x-header></x-header>
-    <div class="loginform">
-        @if ($message = session('success'))
-            <div class="alert alert-success">
-                {{ $message }}
+    <div class="verification-form">
+
+
+        <h2>Reset Password</h2>
+        <form class='form' method="post" action="/sendOtpFaculty">
+            @csrf
+            <label for="faculty_id">Faculty ID</label>
+            <input type="text" name="faculty_id"><br>
+            <input type="submit" name="send_otp" value="Send OTP"><br>
+        </form>
+        @if (session('error'))
+            <div class="alert alert-danger" style="color:red;">
+                {{ session('error') }}
             </div>
         @endif
-        <h2>Login Form</h2>
-        <form class='form' method="post" action="verify">
-            @csrf
-            {{session()->start()}}
-            <label for="student_id" name="username">Username</label>
-            <input type="text" name="student_id" ><br><br>
-            <label for="password" name="password">Password</label>
-            <input type="password" name="password"><br><br>
-            <input type="submit" name="submit" value="Login"><br><br>
-        </form>
-        <a class="forgot-password" href="/otpVerification">Forgot Password?</a><br><br>
-        @if (session('error'))
-        <div class="alert alert-danger" style="color:red;">
-            {{ session('error') }}
-            {{session()->forget('error')}}
-
-        </div>
-    @endif
     </div>
     <x-footer></x-footer>
 </body>
+
 </html>
