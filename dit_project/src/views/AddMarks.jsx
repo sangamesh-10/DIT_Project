f7197c5be3f4ae15e247057c6c3157c9a6b8c690
Your Name <you@example.com>
2023-08-08 21:29:20 +0530

    add marks

--- dit_project/src/views/AddMarks.jsx
+++ dit_project/src/views/AddMarks.jsx
@@ -1,58 +1,89 @@
-import React,{useRef,useState,useEffect} from "react";
+import React, { useRef, useState, useEffect } from "react";
 import axiosClient from "../axios-client";
 
-export const AddMarks=()=>
-{
+export const AddMarks = () => {
     const [subjects, setSubjects] = useState([]);
-    const [selectedSubjects, setSelectedSubjects] = useState([]);
+    const [selectedSubject, setSelectedSubject] = useState('');
     const [enrolledStudents, setEnrolledStudents] = useState([]);
+    const [studentMarks, setStudentMarks] = useState([])
 
     useEffect(() => {
         fetchSubjects();
-      }, []);
+    }, []);
     const fetchSubjects = async () => {
         try {
-          const response = await axiosClient.get("/getFacultySubjects");
-          setSubjects(response.data);
-          setSelectedSubjects([]);
+            const response = await axiosClient.get("/getFacultySubjects");
+            setSubjects(response.data);
+            //setSelectedSubject('');
         } catch (error) {
-          console.error('Error fetching subjects:', error);
+            console.error('Error fetching subjects:', error);
         }
-      };
-      const handleSubjectClick = async (subject) => {
+    };
+    const handleSubjectClick = async (subject) => {
         window.alert(subject);
+        setSelectedSubject(subject);
         try {
-          const response = await axiosClient.get(`/getEnrolledStudents?subject_code=${subject}`);
-          setEnrolledStudents(response.data);
-        } catch (error) {
-          console.error('Error fetching students:', error);
-        }
-      };
-      console.log(enrolledStudents);
+            const response = await axiosClient.get(`/getEnrolledStudents?subject_code=${subject}`);
+            setEnrolledStudents(response.data);
+            setStudentMarks(enrolledStudents.map(rollNum => ({ rollNum, marks: 0 })))
 
-    return(
+        } catch (error) {
+            console.error('Error fetching students:', error);
+        }
+    };
+    const handleMarksChange = (rollNum, newMarks) => {
+        //console.log("hie");
+        setStudentMarks(prevStudentMarks =>
+            prevStudentMarks.map(studentMark =>
+                studentMark.rollNum === rollNum ? { ...studentMark, marks: newMarks } : studentMark
+            )
+        );
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault;
+        console.log(selectedSubject);
+        const payload = {
+            subject_code: selectedSubject,
+            students: studentMarks
+        }
+        console.log(payload);
+        try {
+            const { data } = await axiosClient.post("/addInternalMarks", payload)
+            console.log(data);
+        }
+        catch (err) {
+            console.error("Error while submitting data",err);
+        }
+    }
+
+    return (
         <div>
             <label>Subjects:</label>
             <div>
                 {subjects.map(subject => (
-                <button key={subject} className={selectedSubjects.includes(subject) ? 'selected' : ''}
-                onClick={() => handleSubjectClick(subject)}>{subject} </button>))}
+                    <button
+                        key={subject}
+                        className={selectedSubject === subject ? 'selected' : ''}
+                        onClick={() => handleSubjectClick(subject)}
+                    >
+                        {subject}
+                    </button>
+                ))}
             </div>
-        {enrolledStudents.length > 0 && (
-        <div>
-          <h2>Add Marks</h2>
-          {enrolledStudents.map(student => (
-            <div key={student}>
-            <label>{student} : </label>
-            <input type="number"/>
-          </div>
-        ))}
-          <br />
-          <button>Submit Marks</button>
-        </div>
-      )}
+            <div>
+                {studentMarks.map(studentMark => (
+                    <div key={studentMark.rollNum}>
+                        <label >{studentMark.rollNum}:</label>
+                        <input type="number" value={studentMark.marks}
+                            onChange={e => handleMarksChange(studentMark.rollNum, e.target.value)}
+                        />
+                    </div>
+                ))}
+                <button onClick={onSubmit}>Submit Marks</button>
+            </div>
 
-      </div>
+        </div>
     )
 
 }
